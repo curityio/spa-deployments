6b2eae19b5d2f2b7d03d42231288fecb5b07920c
gary <garyarcher36@gmail.com>
2022-12-29 16:48:41 +0000

    Updates

--- build.sh
+++ build.sh
@@ -1,68 +1,66 @@
 #!/bin/bash
 
-#####################################################################
-# A script to build Token Handler resources for the standard scenario
-#####1###############################################################
+###########################################
+# A script to build Token Handler resources
+###########################################
 
 #
 # Ensure that we are in the folder containing this script
 #
 cd "$(dirname "${BASH_SOURCE[0]}")"
-
-#
-# This is for Curity developers only
-#
 cp ./hooks/pre-commit ./.git/hooks
 
 #
-# Get the scenario to build
+# Get the OAuth agent and default to Node.js
 #
-if [ "$1" == 'financial' ]; then
-  SCENARIO='financial'
-else
-  SCENARIO='standard'
+OAUTH_AGENT="$1"
+if [ "$OAUTH_AGENT" == '' ]; then
+  OAUTH_AGENT="NODE"
 fi
-
-#
-# Different reverse proxies use different plugins and configuration techniques
-#
-if [ "$2" == 'nginx' ]; then
-  REVERSE_PROXY_PROFILE='NGINX'
-elif [ "$2" == 'openresty' ]; then
-  REVERSE_PROXY_PROFILE='OPENRESTY'
-else
-  REVERSE_PROXY_PROFILE='KONG'
+if [ "$OAUTH_AGENT" != 'NODE' ] && [ "$OAUTH_AGENT" != 'NET' ] && [ "$OAUTH_AGENT" != 'KOTLIN' ] && [ "$OAUTH_AGENT" != 'FINANCIAL' ]; then
+  echo 'An invalid value was supplied for the OAUTH_AGENT parameter'
+  exit 1
 fi
-echo "Building resources for the $SCENARIO scenario using $REVERSE_PROXY_PROFILE reverse proxy ..."
+echo "Building resources for the $OAUTH_AGENT OAuth agent ..."
 
 #
-# Build the reverse proxy's custom dockerfile
+# Get the API gateway and OAuth proxy plugin to use, and default to Kong
 #
-cd components/reverse-proxy
-if [ "$REVERSE_PROXY_PROFILE" == 'NGINX' ]; then
+OAUTH_PROXY="$2"
+if [ "$OAUTH_PROXY" == '' ]; then
+  OAUTH_PROXY="KONG"
+fi
+if [ "$OAUTH_PROXY" != 'KONG' ] && [ "$OAUTH_PROXY" != 'NGINX' ] && [ "$OAUTH_PROXY" != 'OPENRESTY' ]; then
+  echo 'An invalid value was supplied for the OAUTH_PROXY parameter'
+  exit 1
+fi
+echo "Building resources for the $OAUTH_PROXY API gateway and plugin ..."
 
-  # Use NGINX if specified on the command line
+#
+# Build the API gateway's custom dockerfile, which includes the OAuth proxy plugin
+#
+cd components/api-gateway
+if [ "$OAUTH_PROXY" == 'NGINX' ]; then
+
   docker build --no-cache -f nginx/Dockerfile -t custom_nginx:1.21.6-alpine .
   if [ $? -ne 0 ]; then
-    echo "Problem encountered building the NGINX Reverse Proxy Docker file"
+    echo "Problem encountered building the NGINX docker image"
     exit 1
   fi
   
-elif [ "$REVERSE_PROXY_PROFILE" == 'OPENRESTY' ]; then
+elif [ "$OAUTH_PROXY" == 'OPENRESTY' ]; then
 
-  # Use OpenResty if specified on the command line
   docker build --no-cache -f openresty/Dockerfile -t custom_openresty/openresty:1.21.4.1-bionic .
   if [ $? -ne 0 ]; then
-    echo "Problem encountered building the OpenResty Reverse Proxy Docker file"
+    echo "Problem encountered building the OpenResty docker image"
     exit 1
   fi
 
-else
+elif [ "$OAUTH_AGENT" == 'KONG' ]; then
   
-  # Use Kong by default
   docker build --no-cache -f kong/Dockerfile -t custom_kong:3.0.0 .
   if [ $? -ne 0 ]; then
-    echo "Problem encountered building the Kong Reverse Proxy Docker file"
+    echo "Problem encountered building the Kong docker image"
     exit 1
   fi
 fi
@@ -71,16 +69,16 @@ cd ..
 #
 # Get and build the OAuth Agent
 #
-if [ "$SCENARIO" == 'standard' ]; then
+rm -rf oauth-agent
+if [ "$OAUTH_AGENT" == 'NODE' ]; then
 
-  # Use the Node.js standard OAuth Agent
-  rm -rf oauth-agent
   git clone https://github.com/curityio/oauth-agent-node-express oauth-agent
   if [ $? -ne 0 ]; then
     echo "Problem encountered downloading the OAuth Agent"
     exit 1
   fi
   cd oauth-agent
+  git checkout feature/pme-778-behaviors
 
   npm install
   if [ $? -ne 0 ]; then
@@ -94,21 +92,21 @@ if [ "$SCENARIO" == 'standard' ]; then
     exit 1
   fi
 
-  docker build -f Dockerfile -t oauthagent-standard:1.0.0 .
-  if [ $? -ne 0 ]; then
-    echo "Problem encountered building the OAuth Agent Docker file"
-    exit 1
-  fi
-else
+elif [ "$OAUTH_AGENT" == 'NET' ]; then
 
-  # Use the Kotlin financial grade OAuth Agent
+  echo '*** The OAuth agent for .NET is not ready yet'
+  exit 1
+
+elif [ "$OAUTH_AGENT" == 'KOTLIN' ]; then
+
   rm -rf oauth-agent
-  git clone https://github.com/curityio/oauth-agent-kotlin-spring-fapi oauth-agent
+  git clone https://github.com/curityio/oauth-agent-kotlin-spring oauth-agent
   if [ $? -ne 0 ]; then
     echo "Problem encountered downloading the OAuth Agent"
     exit 1
   fi
   cd oauth-agent
+  git checkout dev
 
   ./gradlew bootJar
   if [ $? -ne 0 ]; then
@@ -116,10 +114,24 @@ else
     exit 1
   fi
 
-  docker build -f Dockerfile -t oauthagent-financial:1.0.0 .
+elif [ "$OAUTH_AGENT" == 'FINANCIAL' ]; then
+  
+  git clone https://github.com/curityio/oauth-agent-kotlin-spring-fapi oauth-agent
   if [ $? -ne 0 ]; then
-    echo "Problem encountered building the OAuth Agent Docker file"
+    echo "Problem encountered downloading the OAuth Agent"
     exit 1
   fi
-  cd ..
+  cd oauth-agent
+  git checkout feature/pme-778-behaviors
+
+  ./gradlew bootJar
+  if [ $? -ne 0 ]; then
+    echo "Problem encountered building the OAuth Agent's Java code"
+    exit 1
+  fi
+fi
+docker build -f Dockerfile -t oauthagent:1.0.0 .
+if [ $? -ne 0 ]; then
+  echo "Problem encountered building the OAuth Agent Docker file"
+  exit 1
 fi
